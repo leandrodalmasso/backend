f8ed64839f486cad80a3a10b4f2b668168e5976e
leandrodalmasso <leandrodalmasso@gmail.com>
2024-09-16 17:33:23 -0300

    Video 75 static files and middleware

--- express-first-app/index.js
+++ express-first-app/index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const app = express();
 
+// middleware
+app.use('/public', express.static('public'));
+
 app.get("/", (req, res) => {
   res.send(
-    "<html><head><title>My first Express app</title></head><body><h1>Hello world!</h1></body></html>"
+    "<html><link href='/public/styles.css' rel='stylesheet'/><head><title>My first Express app</title></head><body><h1>Hello world!</h1></body></html>"
   );
 });
 
