59d864ab0b86d569a03805755cfe104133a5452d
leandrodalmasso <leandrodalmasso@gmail.com>
2024-09-26 17:08:58 -0300

    77 querystring and post

--- ejs-app/index.js
+++ ejs-app/index.js
@@ -1,9 +1,15 @@
 const express = require("express");
+const bodyParser = require('body-parser');
+
 const app = express();
 
 app.set("view engine", "ejs");
 
+// Serve static files
 app.use("/public", express.static("public"));
+
+// Middleware to parse URL-encoded data
+const urlEncoded = bodyParser.urlencoded({ extended: true });
 
 app.get("/", (req, res) => {
   res.render("index");
@@ -11,6 +17,11 @@ app.get("/", (req, res) => {
 
 app.get("/person/:name", (req, res) => {
   res.render("person", { name: req.params.name });
+});
+
+app.post("/person", urlEncoded, (req, res) => {
+  console.log(req.body);
+  res.send('Form data received!');
 });
 
 const port = process.env.PORT || 3001;
